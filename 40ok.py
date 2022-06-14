72a9740712047abc0188eb4b5d5b4473a997ba1e
C110156208 <102136406+C110156208@users.noreply.github.com>
2022-06-14 10:42:27 +0800

    Add files via upload

---
+++ 40ok.py
@@ -0,0 +1,18 @@
+n = int(input('輸入整數n：'))
+for i in range(2):
+    if i == 0 :
+        for j in range(1,n+1,2):
+            if j % n == 0:
+                for k in range(1,n+1,2):
+                    print(k,end='')
+                for k in range(n-2,0,-2):
+                    print(k,end='')
+            else:
+                print(' '*(n-1),end='')
+                print(j)
+        print()
+    else:
+        for j in range(n-2,0,-2):
+            print(' '*(n-1),end='')
+            print(j)
+        print()
---
+++ 43ok.py
@@ -0,0 +1,6 @@
+a,n=input("").split(" ")
+a=int(a)
+n=int(n)
+a*=n
+n-=1
+print("%dx**%d"%(a,n))
